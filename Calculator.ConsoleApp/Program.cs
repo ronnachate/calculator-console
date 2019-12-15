e9ae77a5571eb81755490dbabc239e81eca7786b
Ronnachate <ronnachate@gmail.com>
2019-12-16 00:13:31 +0700

    Add sample assetment data testing.

--- Calculator.ConsoleApp/Program.cs
+++ Calculator.ConsoleApp/Program.cs
@@ -18,16 +18,31 @@ namespace Calculator.ConsoleApp
             var discountConfig = new Discount();
             config.GetSection("Discount").Bind(discountConfig);
             var calculator = new PriceCalculator(discountConfig);
-            var bestRule = calculator.GetBestRule(2, "STARCARD", 2000 );
+            //due to  important part is Price calculator model, so skip input args and validation here
+            //assume all data is correct and invalid data will be reject before this
+            Console.WriteLine("-- Start Calculate input");
+            Console.WriteLine("Test with 1 person, code DIS10, price 500");
+            var bestRule = calculator.GetBestRule(1, "DIS10", 500 );
             if( !string.IsNullOrEmpty(bestRule.Name))
             {
-                Console.WriteLine( "Match with promotion : " + bestRule.Name.ToString());
-                Console.WriteLine( "Amount : " + bestRule.Price.ToString());
+                Console.WriteLine( "-Match with promotion : " + bestRule.Name.ToString());
+                Console.WriteLine("-Amount : " + bestRule.Price.ToString());
             }
-            else
+            Console.WriteLine("Test with 1 person, code STARCARD, price 2000");
+            bestRule = calculator.GetBestRule(1, "STARCARD", 2000 );
+            if( !string.IsNullOrEmpty(bestRule.Name))
             {
-                Console.WriteLine( "Amount : " + bestRule.Price.ToString());
+                Console.WriteLine("-Match with promotion : " + bestRule.Name.ToString());
+                Console.WriteLine("-Amount : " + bestRule.Price.ToString());
             }
+            Console.WriteLine("Test with 2 person, code DIS10, price 1500");
+            bestRule = calculator.GetBestRule(2, "DIS10", 1500 );
+            if( !string.IsNullOrEmpty(bestRule.Name))
+            {
+                Console.WriteLine("-Match with promotion : " + bestRule.Name.ToString());
+                Console.WriteLine("-Amount : " + bestRule.Price.ToString());
+            }
+            Console.WriteLine("-- End Calculation");
         }
     }
 }
