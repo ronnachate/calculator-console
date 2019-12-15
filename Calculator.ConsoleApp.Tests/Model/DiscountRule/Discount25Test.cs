08b08a21ea3d35c943d332ff825992a02c3a19c1
Ronnachate <ronnachate@gmail.com>
2019-12-15 23:40:13 +0700

    Add discount 25% model.

---
+++ Calculator.ConsoleApp.Tests/Model/DiscountRule/Discount25Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+using Calculator.ConsoleApp.Model.DiscountRule;
+namespace Calculator.ConsoleAppTest.Model.DiscountRule
+{
+    public class Discount25Test
+    {
+        private Discount25 _rule = new Discount25();
+        private readonly decimal MINIMUM_AMOUNT = 2500;
+        [Fact]
+        public void ValidWithMinimumAmountTest()
+        {
+            Assert.True(_rule.IsValid(1, "Test", MINIMUM_AMOUNT ));
+        }
+        [Fact]
+        public void InValidTest()
+        {
+            Assert.False(_rule.IsValid(1, "Test", 1999 ));
+        }
+        [Fact]
+        public void GetNetPriceReturnDiscountedAmountTest()
+        {
+            var netPrice = _rule.GetNetPrice(1, "Test", 3000);
+            Assert.Equal(2250, netPrice);
+        }
+        [Fact]
+        public void GetNetPriceReturnSameForInvalidInputAmountTest()
+        {
+            var netPrice = _rule.GetNetPrice(1, "Test", 1000);
+            Assert.Equal(1000, netPrice);
+        }
+    }
+}
---
+++ Calculator.ConsoleApp/Model/DiscountRule/Discount25.cs
@@ -0,0 +1,24 @@
+﻿namespace Calculator.ConsoleApp.Model.DiscountRule
+{
+    public class Discount25 : BaseDiscount, IDiscountRule
+    {
+        private readonly decimal MINIMUM_AMOUNT = 2500;
+        private readonly decimal DISCOUNT_PERCENTAGE = 25;
+        public decimal GetNetPrice(int customerCount, string couponCode, decimal amount)
+        {
+            if( IsValid(customerCount, couponCode, amount) )
+            {
+                return amount - ( (amount/100) * DISCOUNT_PERCENTAGE );
+            }
+            return amount;
+        }
+        public bool IsValid(int customerCount, string couponCode, decimal amount)
+        {
+            if( amount >= MINIMUM_AMOUNT )
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
